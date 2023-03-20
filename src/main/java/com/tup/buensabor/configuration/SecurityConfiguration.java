0ea3122ff284ade74b35cb42900083cfb6b322a5
Octavio Fariña <octaviofarinia@gmail.com>
2023-03-19 22:26:19 -0300

    first attempt - cors and security configured

--- src/main/java/com/tup/buensabor/configuration/SecurityConfiguration.java
+++ src/main/java/com/tup/buensabor/configuration/SecurityConfiguration.java
@@ -2,9 +2,15 @@ package com.tup.buensabor.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 
 @Configuration
@@ -14,11 +20,26 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-                .requestMatchers("/api/private").authenticated()
-                .anyRequest().permitAll()
-                .and().cors()
+                .requestMatchers(HttpMethod.OPTIONS).permitAll()
+                .requestMatchers("/api/public").permitAll()
+                .requestMatchers("/**").authenticated()
+                .and().cors().configurationSource(corsConfigurationSource())
                 .and().oauth2ResourceServer().jwt();
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PATCH", "PUT", "DELETE", "OPTIONS", "HEAD"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Requestor-Type"));
+        configuration.setExposedHeaders(Arrays.asList("X-Get-Header"));
+        configuration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
