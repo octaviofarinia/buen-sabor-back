de5b24e5443b39a1a096ea772bcf5d56b81365ef
Octavio Fariña <octaviofarinia@gmail.com>
2023-07-25 16:21:41 -0300

    enable async

--- src/main/java/com/tup/buensabor/BuensaborApplication.java
+++ src/main/java/com/tup/buensabor/BuensaborApplication.java
@@ -3,7 +3,9 @@ package com.tup.buensabor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 @ComponentScan({"com.tup.buensabor.mappers", "com.tup.buensabor"})
 public class BuensaborApplication {
--- src/main/java/com/tup/buensabor/services/MailService.java
+++ src/main/java/com/tup/buensabor/services/MailService.java
@@ -3,6 +3,7 @@ package com.tup.buensabor.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,7 @@ public class MailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Async
     public void sendEmail(String to, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
