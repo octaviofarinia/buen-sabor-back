6e06c4010d389f9ee34ec7348bfc153c21b91ce5
Octavio Fariña <octaviofarinia@gmail.com>
2023-07-25 23:06:46 -0300

    email alta pedido improvement

--- src/main/java/com/tup/buensabor/services/MailService.java
+++ src/main/java/com/tup/buensabor/services/MailService.java
@@ -1,10 +1,15 @@
 package com.tup.buensabor.services;
 
+import com.tup.buensabor.entities.Pedido;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.time.ZoneId;
+import java.util.TimeZone;
 
 @Service
 public class MailService {
@@ -19,6 +24,26 @@ public class MailService {
         message.setSubject(subject);
         message.setText(body);
         mailSender.send(message);
+    }
+
+    public void sendAltaPedidoEmail(String to, Pedido pedido) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        dateFormat.setTimeZone(TimeZone.getTimeZone(ZoneId.of("America/Argentina/Buenos_Aires")));
+        String strHoraEstimadaFinalizacion = dateFormat.format(pedido.getHoraEstimadaFinalizacion());
+        this.sendEmail(
+                to,
+                "Alta pedido confirmada Buen Sabor",
+                "¡Hola " + pedido.getCliente().getUsuario().getUsername() + "!\n\n" +
+                        "Tu pedido ha sido confirmado en Buen Sabor.\n" +
+                        "Detalles del pedido:\n" +
+                        "Hora estimada de finalización: " + strHoraEstimadaFinalizacion + "\n" +
+                        "Total: $" + pedido.getTotal() + "\n" +
+                        "Estado del pedido: " + pedido.getEstado() + "\n" +
+                        "Tipo de envío: " + pedido.getTipoEnvio() + "\n" +
+                        "Forma de pago: " + pedido.getFormaPago() + "\n" +
+                        "\nGracias por elegir Buen Sabor. ¡Esperamos que disfrutes tu pedido!\n" +
+                        "Atentamente,\n" +
+                        "El equipo de Buen Sabor");
     }
 
 }
